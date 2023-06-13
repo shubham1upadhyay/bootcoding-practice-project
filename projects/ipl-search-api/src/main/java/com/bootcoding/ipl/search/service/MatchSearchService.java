3bf4b679a78d278f81b69918d77a2d27fa95b0fa
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-14 03:17:11 +0530

    code updated with function to remove the single quotes from the players name

--- projects/ipl-search-api/src/main/java/com/bootcoding/ipl/search/service/MatchSearchService.java
+++ projects/ipl-search-api/src/main/java/com/bootcoding/ipl/search/service/MatchSearchService.java
@@ -1,5 +1,4 @@
 package com.bootcoding.ipl.search.service;
-
 import com.bootcoding.ipl.model.Match;
 import com.bootcoding.ipl.search.query.QueryMapper;
 import com.bootcoding.ipl.search.repository.MatchSearchRepository;
@@ -19,14 +18,22 @@ public class MatchSearchService {
     private MatchSearchRepository searchRepository;
     private List<QueryMapper> queryMappers;
 
+
     public List<Match> getAllMatches(){
         List<Match> matches = searchRepository.findAll();
-
-        for(Match match : matches){
+        for(Match match : matches) {
             match.setVenue(StringUtility.removeDoubleQuote(match.getVenue()));
             match.setTossWinnerTeam(StringUtility.removeDoubleQuote(match.getTossWinnerTeam()));
-        }
 
+              //   method to remove the single quotes
+            for(int i=0; i<match.getTeam1().getPlayers().toArray().length &&
+               i<match.getTeam2().getPlayers().toArray().length; i++)
+            {
+                match.getTeam1().getPlayers().get(i).setName(StringUtility.removeSingleQuotes(match.getTeam1().getPlayers().get(i).getName()));
+                match.getTeam2().getPlayers().get(i).setName(StringUtility.removeSingleQuotes(match.getTeam2().getPlayers().get(i).getName()));
+
+            }
+        }
         return matches;
     }
 
