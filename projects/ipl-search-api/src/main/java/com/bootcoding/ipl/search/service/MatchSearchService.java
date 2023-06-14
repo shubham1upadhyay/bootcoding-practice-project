039ce7952517266b5b4e96d8fd4bb713f4809ae1
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-14 13:34:09 +0530

    problem fixed at "Player of the match name" and "winning team name"

--- projects/ipl-search-api/src/main/java/com/bootcoding/ipl/search/service/MatchSearchService.java
+++ projects/ipl-search-api/src/main/java/com/bootcoding/ipl/search/service/MatchSearchService.java
@@ -1,5 +1,4 @@
 package com.bootcoding.ipl.search.service;
-
 import com.bootcoding.ipl.model.Match;
 import com.bootcoding.ipl.search.query.QueryMapper;
 import com.bootcoding.ipl.search.repository.MatchSearchRepository;
@@ -19,14 +18,28 @@ public class MatchSearchService {
     private MatchSearchRepository searchRepository;
     private List<QueryMapper> queryMappers;
 
+
     public List<Match> getAllMatches(){
         List<Match> matches = searchRepository.findAll();
-
-        for(Match match : matches){
+        for(Match match : matches) {
             match.setVenue(StringUtility.removeDoubleQuote(match.getVenue()));
             match.setTossWinnerTeam(StringUtility.removeDoubleQuote(match.getTossWinnerTeam()));
-        }
 
+            // player of the match
+            match.setPlayerOfTheMatch(StringUtility.removeExtraRegex(match.getPlayerOfTheMatch()));
+
+            //winning team
+            match.setWinningTeam(StringUtility.removeExtraRegex(match.getWinningTeam()));
+
+              //  removing extra quotes from players name
+            for(int i=0; i<match.getTeam1().getPlayers().toArray().length &&
+               i<match.getTeam2().getPlayers().toArray().length; i++)
+            {
+                match.getTeam1().getPlayers().get(i).setName(StringUtility.removeExtraRegex(match.getTeam1().getPlayers().get(i).getName()));
+                match.getTeam2().getPlayers().get(i).setName(StringUtility.removeExtraRegex(match.getTeam2().getPlayers().get(i).getName()));
+
+            }
+        }
         return matches;
     }
 
